ae9c36f03e30bae09b8b7dabc21b1c14f12ebafa
Saúl García <alu0101129785@ull.edu.es>
2021-04-24 20:10:25 +0100

    Documentation utils and interfaces

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,9 +1,14 @@
-
+/**
+ * @interface IndexEntry, entries of the index
+ */
 export interface IndexEntry {
   title: string;
   fileName: string;
 }
 
+/**
+ * @interface NoteIndex, index with all the entries
+ */
 export interface NoteIndex {
   index: IndexEntry[];
 }
--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,12 @@ import chalk from 'chalk';
 import { NoteIndex } from './interfaces';
 import {Note, Color} from './note';
 
+/**
+ * @function getNoteByTitle, checks if a note exists looking for the title
+ * @param title title of the note
+ * @param notes vector of notes
+ * @returns the note if the title exist, or false if the title doesnot appear
+ */
 export function getNoteByTitle(title: string, notes: Note[]) {
   for (const note of notes) {
     if(note.getTitle() === title) {
@@ -11,6 +17,12 @@ export function getNoteByTitle(title: string, notes: Note[]) {
   return false;
 }
 
+/**
+ * @function searchEntryIndex, looks for an entry of the user index
+ * @param title title of the note
+ * @param index index of the user
+ * @returns the correspondent entry to the title given, or false in other case
+ */
 export function searchEntryIndex(title: string, index: NoteIndex) {
   for (const entry of index.index) {
     if(entry.title === title) {
@@ -20,6 +32,13 @@ export function searchEntryIndex(title: string, index: NoteIndex) {
   return false;
 }
 
+/**
+ * @function getColorByString, received a color int a string and returns the
+ * crrespondent color defined into de enum Color
+ * @param color color of the note
+ * @returns the color defined into the enum Color, or false if the color is 
+ * undefined
+ */
 export function getColorByString(color: string) {
   switch(color.toLowerCase()) {
     case 'red':
@@ -42,6 +61,11 @@ export function getColorByString(color: string) {
   }
 }
 
+/**
+ * @function getColorizer, uses the module chalk to paint with differents color
+ * @param note a note of a user
+ * @returns the note paint with the select color, or in white by default
+ */
 export function getColorizer(note: Note) {
   switch(note.getColor()) {
     case Color.RED:
