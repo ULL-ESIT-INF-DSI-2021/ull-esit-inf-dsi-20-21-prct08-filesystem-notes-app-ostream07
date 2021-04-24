9ff1059d614b09c5e1ecfe46782bdb484b81b760
Saúl García <alu0101129785@ull.edu.es>
2021-04-24 17:29:28 +0100

    Pruebas utils

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 
 [Node.js](https://www.npmjs.com/package/@types/node)
 
+// npx ts-node src/index.ts add --user="edusegre" --title="Black note" --body="This is now a black note" --color="black"
+// npx ts-node src/index.ts read --user="edusegre" --title="Yellow note"
+// npx ts-node src/index.ts list --user="edusegre"
+// npx ts-node src/index.ts modify --user="edusegre" --title="Yellow  note" --body="This is now a yellow note" --color="yellow"
+// npx ts-node src/index.ts remove --user="edusegre" --title="Red note"
+
--- tests/fileIO.spec.ts
+++ tests/fileIO.spec.ts
@@ -4,17 +4,17 @@ import { saveNote, loadNotes, removeNote } from "../src/fileIO";
 import { Color, Note } from "../src/note";
 
 describe("fileIO function tests", () => {
-    it("it saves a note and loads it back", () => {
-      const note1 = new Note("note1", Color.YELLOW, "yellow test");
-      const testUser = "testing";
-      saveNote(testUser, note1);
-      expect(loadNotes(testUser)).to.be.deep.equal([note1]);
-    });
-    it("it deletes a note and check that it is removed", () => {
-      const note1 = new Note("note1", Color.YELLOW, "yellow test");
-      const testUser = "testing";
-      saveNote(testUser, note1);
-      removeNote(testUser, note1.getTitle());
-      expect(loadNotes(testUser)).not.contains(note1);
-    });
+  it("it saves a note and loads it back", () => {
+    const note1 = new Note("note1", Color.YELLOW, "yellow test");
+    const testUser = "testing";
+    saveNote(testUser, note1);
+    expect(loadNotes(testUser)).to.be.deep.equal([note1]);
+  });
+  it("it deletes a note and check that it is removed", () => {
+    const note1 = new Note("note1", Color.YELLOW, "yellow test");
+    const testUser = "testing";
+    saveNote(testUser, note1);
+    removeNote(testUser, note1.getTitle());
+    expect(loadNotes(testUser)).not.contains(note1);
+  });
 });
