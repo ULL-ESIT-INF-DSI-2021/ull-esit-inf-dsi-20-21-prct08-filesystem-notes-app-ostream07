bda0dcafacb4da08d44fd61205573b596457d617
Saúl García <alu0101129785@ull.edu.es>
2021-04-24 16:48:21 +0100

    Pruebas utils

--- tests/utils.spec.ts
+++ tests/utils.spec.ts
@@ -11,4 +11,11 @@ describe("utils function tests", () => {
       const vectorTest = [note1, note2, note3];
       expect(getNoteByTitle("note1", vectorTest)).to.be.deep.equal(note1);
     });
+    it("it search the title of a vector with some notes", () => {
+        const note1 = new Note("note1", Color.YELLOW, "yellow test");
+        const note2 = new Note("note2", Color.GREEN, "green test");
+        const note3 = new Note("note3", Color.RED, "red test");
+        const vectorTest = [note1, note2, note3];
+        expect(getNoteByTitle("note4", vectorTest)).to.be.false;
+      });
 });
